002b857898676699e3af90a2345cc0c28c650a34
Azriel Hoh <azriel91@gmail.com>
2020-05-01 16:49:11 +1200

    Added `mermaid` library files.

---
+++ mermaid/mermaid-init.js
@@ -0,0 +1,12 @@
+mermaid.initialize({
+    startOnLoad: true,
+    // https://github.com/mermaid-js/mermaid/issues/385
+    sequence: {
+        diagramMarginX: 0,
+        diagramMarginY: 10,
+        boxTextMargin: 5,
+        noteMargin: 10,
+        messageMargin: 35,
+        // mirrorActors: true
+    },
+});
